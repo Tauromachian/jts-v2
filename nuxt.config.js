1bcf4fe84fca9ecc5c0af1948b93465692a73483
Jose Garcia <jogarcia@tuta.io>
2022-03-26 08:07:39 -0400

    Add meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,55 @@ export default {
   head: {
     title: 'jts-v2',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'keywords', content: 'turbine,inventory,repair,service,GG4,FT4' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Michael Johnson' },
+      { name: 'title', content: 'Johnson Turbine Support' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'We support the GG4 & FT4 market with an extensive inventory of parts, component repair services, engine and turbine overhaul, and field service support.',
+      },
+      { name: 'og:title', content: 'Johnson Turbine Support' },
+      { name: 'og:url', content: 'https://www.johnsonturbine.com/' },
+      {
+        name: 'og:image',
+        content: 'https://www.johnsonturbine.com/images/johnson-logo-min.png',
+      },
+      { name: 'og:type', content: 'website' },
+
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content:
+          'We support the GG4 & FT4 market with an extensive inventory of parts, component repair services, engine and turbine overhaul, and field service support.',
+      },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300&family=Montserrat:wght@700&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -46,6 +75,5 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
