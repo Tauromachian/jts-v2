422e2cf1115cbf3e8738dcf56c7705168c3aaf1f
Jose Garcia <jogarcia@tuta.io>
2022-03-26 09:26:24 -0400

    Fix navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,14 +5,14 @@
         <!-- navigation -->
         <nav class="navbar navbar-expand-lg">
           <div class="container">
-            <a href="index.html" class="navbar-brand">
+            <nuxt-link to="/" class="navbar-brand">
               <img
                 class="img-fluid"
                 src="/johnson-logo-min.png"
                 alt="JTS Logo"
                 width="500"
               />
-            </a>
+            </nuxt-link>
             <button
               id="navbar-toggler"
               class="navbar-toggler mt-3 btn btn-outline-light"
@@ -32,26 +32,22 @@
                 style="--bs-scroll-height: 100px"
               >
                 <li class="nav-item">
-                  <a
-                    class="nav-link active"
-                    aria-current="page"
-                    href="index.html"
-                  >
+                  <nuxt-link class="nav-link active" aria-current="page" to="/">
                     Home
-                  </a>
+                  </nuxt-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="parts.html">Parts</a>
+                  <nuxt-link to="/parts" class="nav-link">Parts</nuxt-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="repair.html">Repair</a>
+                  <nuxt-link to="/repair" class="nav-link">Repair</nuxt-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="service.html">Service</a>
+                  <nuxt-link to="/service" class="nav-link">Service</nuxt-link>
                 </li>
 
                 <li class="nav-item">
-                  <a class="nav-link" href="contact.html">Contact</a>
+                  <nuxt-link to="/contact" class="nav-link">Contact</nuxt-link>
                 </li>
               </ul>
             </div>
@@ -60,26 +56,22 @@
                 class="navbar-nav d-flex flex-column justify-content-center align-items-center h-100 w-100 mx-0"
               >
                 <li class="nav-item">
-                  <a
-                    class="nav-link active"
-                    aria-current="page"
-                    href="index.html"
-                  >
+                  <nuxt-link class="nav-link active" aria-current="page" to="/">
                     Home
-                  </a>
+                  </nuxt-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="parts.html">Parts</a>
+                  <nuxt-link class="nav-link" to="/parts">Parts</nuxt-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="repair.html">Repair</a>
+                  <nuxt-link class="nav-link" to="/repair">Repair</nuxt-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="service.html">Service</a>
+                  <nuxt-link class="nav-link" to="/service">Service</nuxt-link>
                 </li>
 
                 <li class="nav-item">
-                  <a class="nav-link" href="contact.html">Contact</a>
+                  <nuxt-link class="nav-link" to="/contact">Contact</nuxt-link>
                 </li>
               </ul>
               <button id="modal-close-button" class="btn position-absolute">
@@ -103,8 +95,10 @@
             </div>
           </h1>
           <div class="d-flex justify-content-center mt-5 call-to-action-button">
-            <a href="contact.html" class="btn btn-primary px-3 px-sm-5 py-sm-3"
-              >Request a quote</a
+            <nuxt-link
+              to="/contact"
+              class="btn btn-primary px-3 px-sm-5 py-sm-3"
+              >Request a quote</nuxt-link
             >
           </div>
         </div>
@@ -118,14 +112,14 @@
       <div
         class="d-flex flex-column justify-content-center align-items-center pt-5"
       >
-        <a href="index.html" class="navbar-brand">
+        <nuxt-link to="/" class="navbar-brand">
           <img
             class="img-fluid"
             src="/johnson-logo-min.png"
             alt="JTS Logo"
             width="300"
           />
-        </a>
+        </nuxt-link>
         <div class="d-flex justify-content-center">
           <a href="https://www.linkedin.com/company/johnsonturbine/"
             ><span class="mdi mdi-linkedin fs-1"></span
@@ -137,11 +131,11 @@
       </div>
       <div class="container">
         <ul class="d-flex flex-wrap justify-content-center footer-list w-100">
-          <li class="mx-3"><a href="index.html">Home</a></li>
-          <li class="mx-3"><a href="parts.html">Parts</a></li>
-          <li class="mx-3"><a href="repair.html">Repair</a></li>
-          <li class="mx-3"><a href="service.html">Service</a></li>
-          <li class="mx-3"><a href="contact.html">Contact</a></li>
+          <li class="mx-3"><nuxt-link to="/">Home</nuxt-link></li>
+          <li class="mx-3"><nuxt-link to="/parts">Parts</nuxt-link></li>
+          <li class="mx-3"><nuxt-link to="/repair">Repair</nuxt-link></li>
+          <li class="mx-3"><nuxt-link to="/service">Service</nuxt-link></li>
+          <li class="mx-3"><nuxt-link to="/contact">Contact</nuxt-link></li>
         </ul>
         <!-- //Footer -->
         <div class="d-flex justify-content-center py-3">
