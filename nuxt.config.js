da20bce424257bb8ac5f726e5d5d86a01a138fb7
Jose Garcia <jogarcia@tuta.io>
2022-03-27 10:42:12 -0400

    Make google analytics plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,10 @@ export default {
   css: ['~/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/bootstrap.client.js'],
+  plugins: [
+    '~/plugins/bootstrap.client.js',
+    '~/plugins/google-analytics.client.js',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/google-analytics.client.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-113136587-1' },
+})
