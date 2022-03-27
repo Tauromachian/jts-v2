cf697dda9cf9c646fdf26b277bde59b722d88ead
Jose Garcia <jogarcia@tuta.io>
2022-03-27 15:37:04 -0400

    Remove option

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: 'jts-v2',
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'jts-v2',
